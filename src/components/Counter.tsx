436b8b118f0a06b0fac041945e028e821c591edb
bobobo-7 <bobobo7official@gmail.com>
2026-04-10 16:47:46 +0800

    test husky

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,7 +6,7 @@ function Counter() {
     <button
       className="mb-6 inline-flex rounded-[5px] border-2 border-transparent bg-(--accent-bg) px-2.5 py-1.25 text-base font-(--mono) text-(--accent) transition-colors hover:border-(--accent-border) focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-(--accent)"
       onClick={() => {
-        setCount((count) => count + 1);
+        setCount((count) => count + 2);
       }}
     >
       Count is {count}
